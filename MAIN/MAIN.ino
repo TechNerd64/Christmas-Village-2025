283c9f8ebd7cc2f35379986ffc0860f3b07e2703
TechNerd64 <jaxonhohns@gmail.com>
2025-10-02 21:52:00 -0500

    Create MAIN.ino

---
+++ MAIN/MAIN.ino
@@ -0,0 +1,15 @@
+// This is the MAIN code for my Christmas Village
+// Please don't make commits unless they are proven and tested in in other projects
+
+//Setup Libraries
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
