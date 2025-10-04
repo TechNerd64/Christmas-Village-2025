8712cdf73af898246bf198b3a3d3124cfc1506bc
TechNerd64 <jaxonhohns@gmail.com>
2025-10-03 21:53:14 -0500

    Added RTC Code
    Allowed the Arduino to read the date and time from the RTC module

--- Testing_Ground/Testing_Ground.ino
+++ Testing_Ground/Testing_Ground.ino
@@ -1,6 +1,12 @@
+//Needed Libraries
+#include <Wire.h> // Necessary for I2C communication
+#include <RTClib.h> // Library for RTC functions
+
+
 // Initiate Components
 const int boardLed = 13;
 int led = 9;
+RTC_DS1307 rtc;
 
 //Variable to set before program start
 int ledState = LOW;
@@ -10,42 +16,76 @@ int fadeAmount = 5;
 // Millis() timing
 unsigned long previousBoard = 0;
 unsigned long previousLed = 0;
+unsigned long previousRTC = 0;
 const long boardInterval = 1000;
 const long LedInterval = 30;
+const long RTCInterval = 1000;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(boardLed, OUTPUT);
   pinMode(led,OUTPUT);
   analogWrite(led, brightness);
+  Wire.begin();
+
+   // Check if the RTC module is connected and working
+  if (! rtc.begin()) {
+    Serial.println("Error: Couldn't find RTC! Check wiring and power.");
+    Serial.flush();
+    // This stops the program if the RTC isn't found
+    while (1) delay(10); 
+  }
   
   Serial.begin(9600);
 }
 
 void loop() {
+  // Code to run every cycle
+  DateTime now = rtc.now();
+  unsigned long currentMillis = millis();
   
   // Onboard LED (Status Light)
-  unsigned long currentMillis = millis();
+  
   if (currentMillis - previousBoard >= boardInterval) {
     if (ledState == LOW) {ledState = HIGH;}
     else { ledState = LOW;}
-    Serial.print("Led is set to ");
-    Serial.println(ledState);
     previousBoard = currentMillis;
     digitalWrite(boardLed, ledState);
   }
   //External LED
  if (currentMillis - previousLed >= LedInterval) {
    brightness = brightness + fadeAmount;
-   Serial.print("Led Brightness is ");
-   Serial.println(brightness);
    if (brightness <= 0 || brightness >= 255) {fadeAmount = -fadeAmount;}
   analogWrite(led, brightness);
   previousLed = currentMillis;
  }
+  //Time in Serial Moniter
+  if (currentMillis - previousRTC >= RTCInterval) {
+    previousRTC = currentMillis;
+    Serial.print(now.year(), DEC);
+    Serial.print("/");
+    Serial.print(now.month(), DEC);
+    Serial.print("/");
+    Serial.print(now.day(), DEC);
+
+    Serial.print(" - ");
+
+     Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  
+  if (now.minute() < 10) {Serial.print('0');} // Add leading zero for minutes
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  
+  if (now.second() < 10) {Serial.print('0');} // Add leading zero for seconds
+  Serial.print(now.second(), DEC);
+  
+  Serial.println(); // Start a new line
+
+  }
  
 
-  // Update Components
+
   
   
   
