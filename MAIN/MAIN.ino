757c151b27c3a11bffe60f73bd6edb58e68f9491
TechNerd64 <jaxonhohns@gmail.com>
2025-11-10 21:46:27 -0600

    Update MAIN.INO
    Began coding lamp flickering, but need to finish code and figure out how to program a random delay with Millis.

--- MAIN/MAIN.ino
+++ MAIN/MAIN.ino
@@ -2,14 +2,26 @@
 // Please don't make commits unless they are proven and tested in in other projects
 
 //Setup Libraries
+#include "Arduino.h";
+
+//Initialize Components
+const int lamp1 = 2;
+const int lamp2 = 3;
+const int lamp3 = 4:
+
+//Millis Timing Var.
+lampTiming = 60;
 
 
 void setup() {
   // put your setup code here, to run once:
+pinmode(lamp1, OUTPUT);
+pinmode(lamp2, OUTPUT);
+pinmode(lamp3, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+unsigned long currentMillis
 }
