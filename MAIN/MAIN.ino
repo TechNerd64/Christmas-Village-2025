1ed25d7af5aa1528086baf8b48f627c6ebf6e4f0
TechNerd64 <jaxonhohns@gmail.com>
2025-11-21 13:54:39 -0600

    Add RTC and LCD countdown to Christmas
    Integrated RTClib and LiquidCrystal_I2C libraries to display a countdown to Christmas on an I2C LCD. Added RTC initialization, error handling, and a Countdown function that updates the display every minute with the days remaining until Christmas.

--- MAIN/MAIN.ino
+++ MAIN/MAIN.ino
@@ -2,20 +2,34 @@
 // Please don't make commits unless they are proven and tested in in other projects
 
 //Setup Libraries
-#include "Arduino.h";
+#include <Arduino.h>;
+#include <Wire.h>;
+#include <RTClib.h>;
+#include <LiquidCrystal_I2C.h>;
+
 
 //Initialize Components
 const int lamp1 = 2;
 const int lamp2 = 3;
 const int lamp3 = 4;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+RTC_DS1307 rtc;
 
 //Millis Timing Var.
+unsigned long currentMillis = 0;
+
 unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
 unsigned long previousMillis3 = 0;
+unsigned long previousRTC = 0;
 unsigned long lamp1Timing = 0;
 unsigned long lamp2Timing = 0;
 unsigned long lamp3Timing = 0;
+const long RTCtiming = 3600000;
+
+//Countdown Timing
+const long targetDate = 1766646000;
+
 
 void setup() {
 //Start Pins
@@ -26,15 +40,37 @@ pinMode(lamp3, OUTPUT);
 //Millis Timing Var.
 randomSeed(1000);
 
+//Wire (I2C) stuff
+Wire.begin();
+lcd.init();
+lcd.backlight();
+Serial.begin(9600);
+
+lcd.print("Loading...");
+
+ // Check if the RTC module is connected and working
+if (! rtc.begin()) {
+    Serial.println("Error: Couldn't find RTC! Check wiring and power.");
+    Serial.flush();
+    // This stops the program if the RTC isn't found
+while (1) delay(10); 
+}
+
+// OPTIONAL: Uncomment the two lines below to SET the RTC time on upload
+ //Serial.println("Setting time on upload...");
+ //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+ //Serial.println("Time is set!");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+currentMillis = millis();
 LampFlicker();
+Countdown();
 }
 
 void LampFlicker() {
-  unsigned long currentMillis = millis();
 
   //Check to update LED flicker
   if(currentMillis - previousMillis1 >= lamp1Timing){
@@ -52,4 +88,22 @@ void LampFlicker() {
     lamp3Timing = random(60);
     analogWrite(lamp3, random(120)+135);
   }
+}
+
+void Countdown() {
+ DateTime now = rtc.now();
+  long elapsedSec = targetDate - now.unixtime();
+  long daysTill = elapsedSec / 86400;
+  if(currentMillis - previousRTC >= 60000) {
+    previousRTC = currentMillis;
+   
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Only ");
+    lcd.print(daysTill + 1);
+    lcd.print(" days");
+    lcd.setCursor(0,1);
+    lcd.print("until Christmas!");
+  }
+
 }
